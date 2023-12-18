963b5dd4dcc60c30e28b6b20adcbc749e151a949
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2023-12-18 23:34:04 +0300

    Start Design Integration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontHomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('front')->name('front.')->group(function () {
+    Route::get('/', FrontHomeController::class)->name('index');
 });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
