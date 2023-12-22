9337d68987033f63d9a2b53fbe976bd185453499
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2023-12-22 22:08:29 +0300

    Add Admin Guard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Back\BackHomeController;
 use App\Http\Controllers\FrontHomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -17,9 +18,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('front')->name('front.')->group(function () {
     Route::get('/', FrontHomeController::class)->middleware('auth')->name('index');
-    Route::view('/login', 'front.auth.login');
-    Route::view('/register', 'front.auth.register');
-    Route::view('/forget-password', 'front.auth.forget-password');
+    // Route::view('/login', 'front.auth.login');
+    // Route::view('/register', 'front.auth.register');
+    // Route::view('/forget-password', 'front.auth.forget-password');
+
+});
+
+// Admin Design
+Route::prefix('back')->name('back.')->group(function () {
+    Route::get('/', BackHomeController::class)->middleware('admin')->name('index');
+    Route::view('/login', 'back.auth.login');
+    Route::view('/register', 'back.auth.register');
+    Route::view('/forget-password', 'back.auth.forget-password');
 
 });
 
